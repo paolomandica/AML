4e39f8f8d98e4d2b56ff16a8e7b7355693c5ffd9
paolomandica <paolomandica.p@gmail.com>
2021-01-06 11:10:44 +0100

    Minor changes to main.py

--- Final_project/main.py
+++ Final_project/main.py
@@ -5,21 +5,6 @@ from learning import train, evaluate
 from utils import show_images, get_config
 
 
-def reduce_gpu():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(
-                logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
-
-
 if __name__ == "__main__":
 
     # reduce_gpu()
@@ -29,6 +14,7 @@ if __name__ == "__main__":
     train(g_pretrained=True, n_trainable=1, generic=False, config=config)
 
     # # evaluation
-    # im_sr, im_hr = evaluate()
+    # im_sr, im_hr, _, _ = evaluate(
+    #     15, landscapes=True, generic=True, land_class=None)
 
     # show_images(im_sr, im_hr)
